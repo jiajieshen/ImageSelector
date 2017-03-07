8d2b898cb64b92f49f8446c9b759a3e542e48262
xin <cong@xin.pm>
2017-03-07 19:04:50 +0800

    0.2.0

--- app/src/main/java/com/fubaisum/imageselector/MyApplication.java
+++ app/src/main/java/com/fubaisum/imageselector/MyApplication.java
@@ -2,9 +2,6 @@ package com.fubaisum.imageselector;
 
 import android.app.Application;
 
-import me.drakeet.library.CrashWoodpecker;
-import me.drakeet.library.PatchMode;
-
 /**
  * Created by sum on 9/1/16.
  */
@@ -18,12 +15,12 @@ public class MyApplication extends Application {
 //        LeakCanary.install(this);
 //        // BlockCanary检测阻塞
 //        BlockCanary.install(this, new BlockCanaryContext()).start();
-
-        CrashWoodpecker.instance()
-                .withKeys("ImageSelector")
-                .setPatchMode(PatchMode.SHOW_LOG_PAGE)
-                .setPassToOriginalDefaultHandler(true)
-                .flyTo(this);
+//
+//        CrashWoodpecker.instance()
+//                .withKeys("ImageSelector")
+//                .setPatchMode(PatchMode.SHOW_LOG_PAGE)
+//                .setPassToOriginalDefaultHandler(true)
+//                .flyTo(this);
     }
 
 }
